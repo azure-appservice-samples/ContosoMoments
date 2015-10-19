f685b86f41f6fec2d2aa7456b6d45b61c76fc390
alexgolesh <alexg@sela.co.il>
2015-10-18 20:13:15 -0700

    Empty Web (for WebJobs) & Empty Web Job added

--- ContosoMoments/src/ContosoMomentsWebAPI/Controllers/AlbumController.cs
+++ ContosoMoments/src/ContosoMomentsWebAPI/Controllers/AlbumController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace ContosoMomentsWebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class AlbumController : Controller
     {
         // GET: api/values
         [HttpGet]
---
+++ ContosoMoments/src/ContosoMomentsWebAPI/Controllers/LikeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoMomentsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class LikeController : Controller
+    {
+        // POST api/like { imageId }
+        [HttpPost]
+        public void Post([FromBody]Guid ImageId)
+        {
+            System.Diagnostics.Debug.WriteLine(ImageId.ToString());
+        }
+    }
+}
