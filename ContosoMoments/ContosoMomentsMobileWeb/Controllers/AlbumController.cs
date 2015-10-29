132af73c03fa20a31af1c9eed052f5ad83c3418b
alexgolesh <alexg@sela.co.il>
2015-10-29 09:58:08 -0700

    ...

--- ContosoMoments/ContosoMomentsMobileWeb/Controllers/AlbumController.cs
+++ ContosoMoments/ContosoMomentsMobileWeb/Controllers/AlbumController.cs
@@ -64,5 +64,76 @@ namespace ContosoMomentsMobileWeb.Controllers
 
             return images.AsEnumerable();
         }
+
+        public async System.Threading.Tasks.Task<bool> Post(string AlbumName)
+        {
+            string baseUrl = System.Configuration.ConfigurationManager.AppSettings["WebAPI_BaseURL_DEBUG"];
+            string url = baseUrl + "album";
+
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            //TODO!!!
+            //string postData = AlbumName;
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            //byte[] bytes = encoding.GetBytes(postData);
+            //request.ContentLength = bytes.Length;
+
+            //using (System.IO.Stream newStream = request.GetRequestStream())
+            //{
+            //    newStream.Write(bytes, 0, bytes.Length);
+            //}
+
+            //var res = await request.GetResponseAsync();
+            //var reponseStream = res.GetResponseStream();
+            //byte[] responseBytes = new byte[res.ContentLength];
+            //await reponseStream.ReadAsync(responseBytes, 0, (int)res.ContentLength);
+            //string s = Encoding.UTF8.GetString(responseBytes, 0, responseBytes.Length);
+
+            return true;
+        }
+
+        public async System.Threading.Tasks.Task Put(string Id, Album Metadata)
+        {
+            string baseUrl = System.Configuration.ConfigurationManager.AppSettings["WebAPI_BaseURL"];
+            string url = baseUrl + "album/" + Id;
+
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.Method = "PUT";
+            request.ContentType = "application/json";
+
+            //TODO - add body with metadata....
+            //string serializedMetadata = Newtonsoft.Json.JsonConvert.SerializeObject(Metadata);
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            //byte[] bytes = encoding.GetBytes(serializedMetadata);
+            //request.ContentLength = bytes.Length;
+
+            //using (System.IO.Stream stream = request.GetRequestStream())
+            //{
+            //    stream.Write(bytes, 0, bytes.Length);
+            //}
+
+            //var res = await request.GetResponseAsync();
+            //var reponseStream = res.GetResponseStream();
+            //byte[] responseBytes = new byte[res.ContentLength];
+            //await reponseStream.ReadAsync(responseBytes, 0, (int)res.ContentLength);
+            //string s = Encoding.UTF8.GetString(responseBytes, 0, responseBytes.Length);
+        }
+
+        public async System.Threading.Tasks.Task Delete(string Id)
+        {
+            string baseUrl = System.Configuration.ConfigurationManager.AppSettings["WebAPI_BaseURL"];
+            string url = baseUrl + "album/" + Id;
+
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.Method = "DELETE";
+
+            var res = await request.GetResponseAsync();
+            var stream = res.GetResponseStream();
+            byte[] bytes = new byte[res.ContentLength];
+            await stream.ReadAsync(bytes, 0, (int)res.ContentLength);
+            string s = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+        }
     }
 }
