07e568158d62097c57788db0ecf001d2e8d1c13b
alexgolesh <alexg@sela.co.il>
2015-10-29 17:58:29 -0700

    Empty Xamarin app added to solution (Droid, iOS, WP8.1)

---
+++ ContosoMoments/Mobile/ContosoMoments/ContosoMoments.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ContosoMoments.WinPhone.Resources;
+
+namespace ContosoMoments.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
