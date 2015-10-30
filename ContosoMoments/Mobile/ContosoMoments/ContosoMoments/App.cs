656d54600aa02855b1937263d7a189ec09c7dfc1
alexgolesh <alexg@sela.co.il>
2015-10-30 07:05:17 -0700

    Added images list page

--- ContosoMoments/Mobile/ContosoMoments/ContosoMoments/App.cs
+++ ContosoMoments/Mobile/ContosoMoments/ContosoMoments/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContosoMoments.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +12,19 @@ namespace ContosoMoments
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            //MainPage = new ContentPage {
+            //	Content = new StackLayout {
+            //		VerticalOptions = LayoutOptions.Center,
+            //		Children = {
+            //			new Label {
+            //				XAlign = TextAlignment.Center,
+            //				Text = "Welcome to Xamarin Forms!"
+            //			}
+            //		}
+            //	}
+            //};
+            MainPage = new ImagesList();
 		}
 
 		protected override void OnStart ()
---
+++ ContosoMoments/Mobile/ContosoMoments/ContosoMoments/Views/ImagesList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ContosoMoments.Views
+{
+	public partial class ImagesList : ContentPage
+	{
+		public ImagesList ()
+		{
+			InitializeComponent ();
+		}
+
+        public async void OnRefresh(object sender, EventArgs e)
+        {
+        }
+
+        public async void OnSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+        }
+    }
+}
