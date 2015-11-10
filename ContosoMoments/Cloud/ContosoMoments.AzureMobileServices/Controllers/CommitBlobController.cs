4a8d8e8d4b1f2daaa2f197a60c214df00631b0c5
Rotem Or <rotemo@sela.co.il>
2015-11-10 22:34:27 +0200

    merge

--- ContosoMoments/Cloud/ContosoMoments.AzureMobileServices/Controllers/CommitBlobController.cs
+++ ContosoMoments/Cloud/ContosoMoments.AzureMobileServices/Controllers/CommitBlobController.cs
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Web.Http;
-using System.Web.Http.Cors;
-using ContosoMoments.Common.Srorage;
-using Microsoft.WindowsAzure.Mobile.Service;
 
-namespace ContosoMoments.AzureMobileServices.Controllers
-{
-    [EnableCors(origins: "*", headers: "*", methods: "*")]
-
-    public class CommitBlobController : ApiController
-    {
-        public ApiServices Services { get; set; }
-
-        // POST: api/Default
-        public bool Post([FromBody]CommitBlobRequest commitBlobRequest)
-        {
-            var cs = new ContosoStorage();
-            return cs.CommitUpload(commitBlobRequest);
-
-        }
-
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,4 +27,4 @@ namespace ContosoMoments.MobileServices.Controllers
 
     }
 }
->>>>>>> e8dea8fbb97f19cfd7c518eb7fe7672598be2304
+
--- ContosoMoments/Cloud/ContosoMoments.AzureMobileServices/Controllers/GetSasUrlController.cs
+++ ContosoMoments/Cloud/ContosoMoments.AzureMobileServices/Controllers/GetSasUrlController.cs
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Web.Http;
-using System.Web.Http.Cors;
-using ContosoMoments.Common.Srorage;
-using Microsoft.WindowsAzure.Mobile.Service;
-
-namespace ContosoMoments.AzureMobileServices.Controllers
-{
-    public class GetSasUrlController : ApiController
-    {
-        public ApiServices Services { get; set; }
-
-
-        [EnableCors(origins: "*", headers: "*", methods: "*")]
-        // GET api/getsasurl
-        public string Get()
-        {
-            Services.Log.Info("Start - GetSasUrlController ");
-            var containerName = "uploadFolder";
-            var fileName = Guid.NewGuid().ToString() + ".jpg";
-            var cs = new ContosoStorage();
-            var result = cs.GetSasUrlAndSetCORS(containerName, fileName);
-            Services.Log.Info("End - GetSasUrlController ");
-
-
-            return   result ;
-
-        }
-
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -66,4 +33,4 @@ namespace ContosoMoments.MobileServices.Controllers
 
     }
 }
->>>>>>> e8dea8fbb97f19cfd7c518eb7fe7672598be2304
+
