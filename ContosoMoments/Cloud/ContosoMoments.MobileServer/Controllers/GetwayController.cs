e0d030185ec1d9d96c04713bafa8b53095ade69c
Rotem Or <rotemo@sela.co.il>
2015-11-12 02:01:22 +0200

    new Getway Controller

---
+++ ContosoMoments/Cloud/ContosoMoments.MobileServer/Controllers/GetwayController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContosoMoments.Common;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace ContosoMoments.MobileServer.Controllers
+{
+    [MobileAppController]
+    public class GetwayController : ApiController
+    {
+        
+
+        // GET api/Getway
+        public string Get()
+        {
+
+            return AppSettings.FacebookAuthString;
+        }
+
+    }
+}
