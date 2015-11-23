a900f529f6b63470631221b9bd65f184505c923b
Rotem Or <rotor@Rotems-MacBook-Pro.local>
2015-11-23 10:37:58 +0200

    New Like Controller as WebAPI

---
+++ ContosoMoments/Cloud/ContosoMoments.MobileServer/Controllers/WebAPI/LikeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContosoMoments.MobileServer.Controllers.WebAPI
+{
+    public class LikeController : ApiController
+    {
+        // POST: api/Like
+        public void Post([FromBody]string imageId)
+        {
+            // 
+        }
+    }
+}
