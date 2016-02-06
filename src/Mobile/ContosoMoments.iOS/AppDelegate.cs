323b33b154e27dbc296f19196c19fd8d84114600
Cory Fowler <cfowler@microsoft.com>
2016-02-06 02:38:38 -0800

    added android and ios build profiles

--- src/Mobile/ContosoMoments.iOS/AppDelegate.cs
+++ src/Mobile/ContosoMoments.iOS/AppDelegate.cs
@@ -55,7 +55,7 @@ namespace ContosoMoments.iOS
 
             LoadApplication(new ContosoMoments.App());
 
-#error COMMENT WHEN DEBUGGING ON EMULATOR!
+            #error COMMENT WHEN DEBUGGING ON EMULATOR!
             var imagePicker = new UIImagePickerController { SourceType = UIImagePickerControllerSourceType.Camera };
             (Xamarin.Forms.Application.Current as App).ShouldTakePicture += () =>
                 app.KeyWindow.RootViewController.PresentViewController(imagePicker, true, null);
