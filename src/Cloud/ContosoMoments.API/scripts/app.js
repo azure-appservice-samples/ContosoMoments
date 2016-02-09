579f63ab9a4144c6a15a3e5f4c2310b73f08c9ae
Rotem Or <rotemml@hotmail.com>
2016-02-09 12:39:32 +0200

    Easy Auth Integration Login Landing page

--- src/Cloud/ContosoMoments.API/scripts/app.js
+++ src/Cloud/ContosoMoments.API/scripts/app.js
@@ -1,7 +1,7 @@
-(function(angular){
+(function (angular) {
     'use strict';
 
-    var app=angular.module('app',['ui.router','ui.bootstrap','azureBlobUpload']);
+    var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'azureBlobUpload']);
 
     app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function ($locationProvider, $stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -9,27 +9,37 @@
                 abstract: true,
                 template: "<ui-view/>",
                 resolve: {
-                    authContext: ['authService',function (authService) {
+                    authContext: ['authService', function (authService) {
                         return authService.getAuthContext();
                     }]
                 }
             })
+
             .state('main.albums', {
                 url: '/',
                 templateUrl: '/templates/albums.html',
                 controller: 'albumsController as albumsCtrl',
-                onEnter:['selectedAlbum',function(selectedAlbum){
-                    selectedAlbum.album=null; 
+                onEnter: ['selectedAlbum', function (selectedAlbum) {
+                    selectedAlbum.album = null;
                 }]
 
             })
+             .state('main.auth', {
+                 url: '/auth',
+                 templateUrl: '/templates/auth.html'
+                 //controller: 'albumsController as albumsCtrl',
+                 // onEnter: ['selectedAlbum', function (selectedAlbum) {
+                 //   selectedAlbum.album = null;
+
+
+             })
             .state('main.gallery', {
                 url: '/album/:albumid',
                 templateUrl: '/templates/gallery.html',
                 controller: 'albumController as albumCtrl',
                 resolve: {
                     currentAlbum: ['albumsService', '$stateParams', function (albumsService, $stateParams) {
-                        return albumsService.getAlbum($stateParams.albumid, { start: 0, count: 24 });   
+                        return albumsService.getAlbum($stateParams.albumid, { start: 0, count: 24 });
                     }]
                 },
                 onEnter: ['selectedAlbum', 'currentAlbum', function (selectedAlbum, currentAlbum) {
@@ -37,22 +47,22 @@
                 }]
             })
             .state('main.singleImage', {
-                url:'/image/:imageid',
+                url: '/image/:imageid',
                 templateUrl: '/templates/singleimage.html',
                 controller: 'imageController as imageCtrl',
                 resolve: {
                     currentImage: ['selectedImage', '$stateParams', 'imageService', function (selectedImage, $stateParams, imageService) {
-                        if (selectedImage.image)  return selectedImage;
+                        if (selectedImage.image) return selectedImage;
                         if ($stateParams.imageid) {
                             return imageService.getImageById($stateParams.imageid);
                         }
                     }]
                 }
             })
-            .state('error',{
+            .state('error', {
                 url: '/error/:code?',
                 template: '<h2 ng-bind="errorText">',
-                controller:['$scope','$stateParams',function($scope,$stateParams){
+                controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
                     var errCode = $stateParams.code || 500;
                     switch (errCode) {
                         case 404:
@@ -67,12 +77,12 @@
             });
 
         $urlRouterProvider.when('/_=_', '/')
-            .when('_-_','/')
+            .when('_-_', '/')
             .when('/index.html', '/')
             .otherwise('/error/404');
-     
+
         $locationProvider.html5Mode(true);
 
     }])
-    
+
 })(angular)
