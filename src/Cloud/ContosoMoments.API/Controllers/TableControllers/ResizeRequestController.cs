1c82e6142f446e86b566ed1bffda9b590f6063b8
Donna Malayeri <donnam@microsoft.com>
2016-02-17 13:57:17 -0800

    Add a controller for ResizeRequest, which is called by the mobile client after image upload is complete

---
+++ src/Cloud/ContosoMoments.API/Controllers/TableControllers/ResizeRequestController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using ContosoMoments.Common;
+using ContosoMoments.Common.Queue;
+using Microsoft.Azure.Mobile.Server;
+
+namespace ContosoMoments.API.Controllers.TableController
+{
+    public class ResizeRequest : EntityData
+    {
+        public string BlobName { get; set; }
+    }
+
+    public class ResizeRequestController : TableController<ResizeRequest>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+        }
+
+        public IHttpActionResult PostImage(ResizeRequest item)
+        {
+            item.Deleted = true;
+            return CreatedAtRoute("Tables", new { id = item.Id }, item);
+        }
+
+        public IEnumerable<ResizeRequest> GetAllImage()
+        {
+            return Enumerable.Empty<ResizeRequest>();
+        }
+
+        private async Task PostToQueue(string blobName)
+        {
+            var fileNameParts = blobName.Split('.');
+            string imageId = fileNameParts[0];
+            string extension = fileNameParts.Length > 1 ? fileNameParts[1] : "";
+
+            var qm = new QueueManager();
+            var blobInfo = new BlobInformation(extension);
+            blobInfo.BlobUri = new Uri(string.Format("https://{0}.blob.core.windows.net", AppSettings.StorageAccountName));
+            blobInfo.ImageId = imageId;
+
+            await qm.PushToResizeQueue(blobInfo);
+            Trace.WriteLine("Sent resize request for blob: " + imageId);
+        }
+    }
+}
