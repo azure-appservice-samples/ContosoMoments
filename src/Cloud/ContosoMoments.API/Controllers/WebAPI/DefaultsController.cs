3d7c34947479e8deb6e49e771b0fb673a6e1ea42
Donna Malayeri <donnam@microsoft.com>
2016-02-22 15:25:38 -0800

    Rename GetwayController to DefaultsController and return default values

---
+++ src/Cloud/ContosoMoments.API/Controllers/WebAPI/DefaultsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Mobile.Server.Config;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using ContosoMoments.Common;
+
+namespace ContosoMoments.MobileServer.Controllers.WebAPI
+{
+    [MobileAppController]
+    public class DefaultsController : ApiController
+    {
+        public JObject GetDefaults()
+        {
+            var result = new JObject();
+            result["DefaultUserId"]  = AppSettings.DefaultUserId;
+            result["DefaultAlbumId"] = AppSettings.DefaultAlbumId;
+
+            return result;
+        }
+    }
+}
