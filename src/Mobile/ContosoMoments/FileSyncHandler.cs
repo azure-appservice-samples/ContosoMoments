695917d51724f1f78516094950997f0c2b86645e
Donna Malayeri <donnam@microsoft.com>
2016-03-01 16:52:18 -0800

    Mobile client: add exception handling in case image download fails

--- src/Mobile/ContosoMoments/FileSyncHandler.cs
+++ src/Mobile/ContosoMoments/FileSyncHandler.cs
@@ -4,6 +4,7 @@ using Microsoft.WindowsAzure.MobileServices.Files;
 using Microsoft.WindowsAzure.MobileServices.Files.Metadata;
 using Xamarin.Forms;
 using System.Diagnostics;
+using System;
 
 namespace ContosoMoments
 {
@@ -24,15 +25,20 @@ namespace ContosoMoments
 
         public async Task ProcessFileSynchronizationAction(MobileServiceFile file, FileSynchronizationAction action)
         {
-            if (action == FileSynchronizationAction.Delete) {
-                await FileHelper.DeleteLocalFileAsync(file, theApp.DataFilesPath);
-            }
-            else { // Create or update. We're aggressively downloading all files.
-                Trace.WriteLine(string.Format("File - storeUri: {1}", file.Name, file.StoreUri));
-
-                if (file.StoreUri.Contains("lg")) {
-                    await this.theApp.DownloadFileAsync(file);                    
+            try {
+                if (action == FileSynchronizationAction.Delete) {
+                    await FileHelper.DeleteLocalFileAsync(file, theApp.DataFilesPath);
                 }
+                else { // Create or update - download large format image by looking for 'lg' in the StoreUri parameter
+                    Trace.WriteLine(string.Format("File - storeUri: {1}", file.Name, file.StoreUri));
+
+                    if (file.StoreUri.Contains("lg")) {
+                        await this.theApp.DownloadFileAsync(file);
+                    }
+                }
+            }
+            catch (Exception e) { // should catch WrappedStorageException, but this type is internal in the Storage SDK!
+                Trace.WriteLine("Exception while downloading blob, blob probably does not exist: " + e);
             }
         }
     }
