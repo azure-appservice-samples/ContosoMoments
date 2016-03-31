c0df7d8b27c4e9146d3126b87352eca966ad93b6
Cory Fowler <cfowler@microsoft.com>
2016-03-30 18:40:00 -0700

    updating with latest changes

--- src/Cloud/ContosoMoments.API/Controllers/TableControllers/ImageStorageController.cs
+++ src/Cloud/ContosoMoments.API/Controllers/TableControllers/ImageStorageController.cs
@@ -1,24 +1,42 @@
 ﻿using ContosoMoments.Common.Models;
+using ContosoMoments.Common;
 using Microsoft.Azure.Mobile.Server.Files;
 using Microsoft.Azure.Mobile.Server.Files.Controllers;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System;
+using Newtonsoft.Json.Linq;
 
 namespace ContosoMoments.Api
 {
     public class ImageStorageController : StorageController<Image>
     {
-        public ImageStorageController() : 
+        public ImageStorageController() :
             base(new CustomAzureStorageProvider())
-        { }
+        {
+            var baseUri = AppSettings.FunctionsBaseUri;
+            TableLogger.Init(baseUri);
+        }
 
         [HttpPost]
         [Route("tables/Image/{id}/StorageToken")]
         // return a storage token that can be used for blob upload or download
         public async Task<HttpResponseMessage> PostStorageTokenRequest(string id, StorageTokenRequest request)
-        {            
+        {
+            TableLogger.LogMessage("ContosoMomentsAPI",
+                Guid.NewGuid().ToString(),
+                new TraceItem
+                {
+                    Api = "StorageToken",
+                    Time = DateTime.UtcNow,
+                    UserId = AppSettings.DefaultUserId,
+                    ImageId = request.TargetFile.Id,
+                    AlbumId = AppSettings.DefaultAlbumId
+                },
+                AppSettings.FunctionPathAndSecret);
+
             StorageToken token = await GetStorageTokenAsync(id, request, new ImageNameResolver(request.TargetFile.StoreUri));
             return Request.CreateResponse(token);
         }
